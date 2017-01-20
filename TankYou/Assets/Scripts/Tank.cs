13c23146a674f5150926cf2ad350dc4566b4a50c
David Vink <david.vink@hotmail.com>
2017-01-20 15:15:12 +0100

    Added Movement

--- TankYou/Assets/Scripts/Tank.cs
+++ TankYou/Assets/Scripts/Tank.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tank : MonoBehaviour {
+    public int ammoCount = 3;
+    public int health = 100;
+    public GameObject bulletPref;
+    public float reloadTime;
+    public float shootInterval;
+    public Transform[] tankParts;
+    bool shoot = true;
+    float turnSpeed;
+    public float turnSpeedO;
+    public float rideSpeed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		
+        Movement();
 	}
+    void Movement() {
+        if(Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical" ) < 0) {
+            turnSpeed = turnSpeedO / 2;
+        }
+        else {
+            turnSpeed = turnSpeedO;
+        }
+        transform.Translate(0, Input.GetAxis("Vertical") * rideSpeed, 0);
+
+
+        transform.Rotate(0, 0, -Input.GetAxis("Horizontal") * turnSpeed);
+    }
+    
 }
