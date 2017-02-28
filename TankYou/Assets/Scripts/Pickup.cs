cad2cb831ce698e6bb0124d3c07765199a118ed2
DeesV <dees.veen@ehldeventer.nl>
2017-02-28 16:20:31 +0100

    Pickup Script
    Start for pickup

---
+++ TankYou/Assets/Scripts/Pickup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    public int pickUpType = 3;
+
+	void PickUpManager() {
+
+        switch (pickUpType) {
+            case 3:
+                print("Case3 works");
+                break;
+            case 2:
+                print("Case2 works");
+                break;
+            case 1:
+                print("Case1 works");
+                break;
+            default:
+                print("Default works");
+                break;
+        }
+    }
+}
